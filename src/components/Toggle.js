6310d824b9f521ec5cdcfc88216ea760063d920f
hoonkim <hoonk9387@gmail.com>
2022-06-04 21:09:39 -0500

    Added state variable to component. Added conditional rendering to JSX element.

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  /* Create state variable.
+     Call useState with initial value.
+     Then array destructure state value and state setter function. */
+  const [isOn, setIsOn] = useState(false);
+
+  return <button>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
